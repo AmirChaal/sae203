b24cd73e34bc8ee3618d7cee4f87776697047276
Amir Chaal <mohamedamir@gmail.com>
2022-03-03 22:32:58 +0100

    normal

--- site/index.php
+++ site/index.php
@@ -11,7 +11,7 @@ require 'header.php';
 	<p>La musique post-industrielle reprend l'esthétique brute, sombre et mécanique de la musique industrielle et y insuffle des influences plus accessibles telles que le rock, le metal ou encore le <a href="http://mmi21d04.h205.online">Folk</a>.</p>
 
 
-	<p>Pour aller plus loin, la chaîne youtube anglophone Theneedledrop a produit une analyse de l'album You Won't Get What You Want du groupe pionnier Daughters. TEST</p>
+	<p>Pour aller plus loin, la chaîne youtube anglophone Theneedledrop a produit une analyse de l'album You Won't Get What You Want du groupe pionnier Daughters.</p>
 	<iframe width="560" height="315" src="https://www.youtube.com/embed/DO5pcLp8-HM" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
 </div>
 
