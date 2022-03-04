8c133be8567f5281b9829091082eab97bdcc631a
Amir Chaal <mohamedamir@gmail.com>
2022-03-04 11:54:55 +0100

    yj

--- debut_html.php
+++ debut_html.php
@@ -17,8 +17,9 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
 	
-	<!-- PARSLEY -->
-	<script src="parsley.min.js"></script>
+	    <!-- PARSLEY -->
+		<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+		<script src="parsley.min.js"></script>
 	
 </head>
 <main>
