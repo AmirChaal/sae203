0980f046df8e6f37013077ff424ae9f3aa418144
Amir Chaal <mohamedamir@gmail.com>
2022-03-04 10:30:35 +0100

    clean

--- site/form_recherche.php
+++ site/form_recherche.php
@@ -5,6 +5,37 @@ require 'debut_html.php';
 require 'header.php';
 ?>
 
+<div class="slider">
+        <div class="progress"></div>
+      </div>
+      <div class="range-input">
+        <input type="range" class="range-min" min="0" max="10000" value="2500" step="100">
+        <input type="range" class="range-max" min="0" max="10000" value="7500" step="100">
+      </div>
+    </div>
+    <script>
+		const rangeInput = document.querySelectorAll(".range-input input"),
+		priceInput = document.querySelectorAll(".price-input input"),
+		range = document.querySelector(".slider .progress");
+		let priceGap = 1000;
+		priceInput.forEach(input =>{
+			input.addEventListener("input", e =>{
+				let minPrice = parseInt(priceInput[0].value),
+				maxPrice = parseInt(priceInput[1].value);
+				
+				if((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max){
+					if(e.target.className === "input-min"){
+						rangeInput[0].value = minPrice;
+						range.style.left = ((minPrice / rangeInput[0].max) * 100) + "%";
+					}else{
+						rangeInput[1].value = maxPrice;
+						range.style.right = 100 - (maxPrice / rangeInput[1].max) * 100 + "%";
+					}
+				}
+			});
+		});
+</script>
+
 <form action="reponse_recherche.php" method="POST" data-parsley-validate>
 	<input type="number" name="anneemin" class="search" placeholder="Annee min" data-parsley-type="number">
 	<input type="number" name="anneemax" class="search" placeholder="Annee max" data-parsley-type="number">
