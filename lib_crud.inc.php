c654637ecbdc1c0658636708081b48d91f23e867
Amir Chaal <mohamedamir@gmail.com>
2022-03-21 12:20:30 +0100

    f

--- lib_crud.inc.php
+++ lib_crud.inc.php
@@ -17,6 +17,27 @@ function deconnexionBD(&$mabd) {
     $mabd=null;
 }
 
-
-
-
+function afficherCatalogue($mabd) {
+    $req = "SELECT * FROM productions INNER JOIN auteurs ON productions._aut_id = auteurs.aut_id";
+    $resultat = $mabd->query($req);
+    foreach ($resultat as $value) {
+        echo    '<div class="mycontainer">
+                    <div class="mycard"> <!-- Card -->
+                        <div class="vignette">
+                            <img src="img/bds/'.$value['prod_photo'].'" alt="'.$value['prod_photo'].'">
+                            <div class="labelB">
+                                <span>Noté '.$value['prod_note'].'/500</span>
+                                <span>Classé '.$value['prod_classement'].'ème</span> 
+                            </div>
+                        </div>
+                        <div class="labelD">
+                            <p>'.$value['prod_nom'].'</p>
+                            <p>'.$value['prod_annee'].'</p>
+                            <p>'.$value['aut_nom'].'</p>
+                            <p>né.e ou formé en '.$value['aut_annee'].', '.$value['aut_lieu'].'</p>
+                            <p>'.$value['prod_inf'].'</p>
+                        </div>
+                    </div>
+                </div>';
+    }
+}
--- listing.php
+++ listing.php
@@ -11,37 +11,15 @@ require 'lib_crud.inc.php';
     <div class="mycards">
 
         <?php
-            $mabd = connexionBD();
-            $req = "SELECT * FROM productions INNER JOIN auteurs ON productions._aut_id = auteurs.aut_id";
-            $resultat = $mabd->query($req);
-
-            foreach ($resultat as $value) {
-                echo    '<div class="mycontainer">
-                            <div class="mycard"> <!-- Card -->
-                                <div class="vignette">
-                                    <img src="img/bds/'.$value['prod_photo'].'" alt="'.$value['prod_photo'].'">
-                                    <div class="labelB">
-                                        <span>Noté '.$value['prod_note'].'/500</span>
-                                        <span>Classé '.$value['prod_classement'].'ème</span> 
-                                    </div>
-                                </div>
-                                <div class="labelD">
-                                    <p>'.$value['prod_nom'].'</p>
-                                    <p>'.$value['prod_annee'].'</p>
-                                    <p>'.$value['aut_nom'].'</p>
-                                    <p>né.e ou formé en '.$value['aut_annee'].', '.$value['aut_lieu'].'</p>
-                                    <p>'.$value['prod_inf'].'</p>
-                                </div>
-                            </div>
-                        </div>';
-            }
+            $co = connexionBD();
+            afficherCatalogue($co)
         ?>
 
     </div>
 </div>
 
 <?php
-deconnexionBD();
+deconnexionBD($co);
 
 require 'footer.php';
 require 'fin_html.php';
